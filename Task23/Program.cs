32124b2b0ef621f878e19328e309250e435f6e6c
Елена <lenna@gmail.com>
2023-08-13 22:05:57 +0300

    Added program in Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+   TableCube(number);
+   
+ 
+}
+
+void TableCube(int num){
+    int count = 1;
+     while (count <= num)
+     {
+        Console.WriteLine($"{count} {count * count * count}" );
+        count++;
+        
+     }
+}
